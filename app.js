f85e2d47a41cd3ff826ae574217b45afcd9cdfe5
adcastiblanco <angelcastiblanco96@gmail.com>
2023-04-11 14:55:04 -0500

    feat(App): create conditional to show inquirer menu

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-import { readInput } from "./helpers/inquirer.js";
+import { inquirerMenu, pause } from "./helpers/inquirer.js";
 
 const main = async () => {
-  const data = await readInput("Hola: ");
-  console.log(data);
+  let opt = "";
+  do {
+    opt = await inquirerMenu();
+    if (opt !== 0) await pause();
+  } while (opt !== 0);
 };
 
 main();
